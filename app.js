4fc9f956f67355ebebcc97410b3faba5baa17336
Marty Nelson <marty@martypdx.com>
2021-12-09 15:29:12 -0800

    add some sounds

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ const rockThrow = document.getElementById('rock-throw');
 rockThrow.addEventListener('click', () => {
     runGame('rock');
 });
+rockThrow.addEventListener('mouseenter', () => {
+    playSound('./assets/sounds/rock.mp3');
+});
+
 const paperThrow = document.getElementById('paper-throw');
 paperThrow.addEventListener('click', () => {
     runGame('paper');
 });
+paperThrow.addEventListener('mouseenter', () => {
+    playSound('./assets/sounds/paper.ogg');
+});
+
 const scissorsThrow = document.getElementById('scissors-throw');
 scissorsThrow.addEventListener('click', () => {
     runGame('scissors');
 });
+scissorsThrow.addEventListener('mouseenter', () => {
+    playSound('./assets/sounds/scissors.wav');
+});
+
+const audio = document.getElementById('audio');
+function playSound(file) {
+    audio.src = file;
+    audio.play();
+}
 
 const results = document.getElementById('results');
 const playerThrow = document.getElementById('player-throw');
@@ -27,9 +44,20 @@ function runGame(player) {
     playerThrow.src = `assets/images/${player}.jpeg`;
     computerThrow.src = `assets/images/${computer}.jpeg`;
     resultImage.src = `assets/images/${result}.jpeg`;
-
+    
     setStatus(playerThrow, result === 'player');
     setStatus(computerThrow, result === 'computer');
+    
+    if (result === 'player') {
+        playSound('./assets/sounds/win.mp3');
+    }
+    else if (result === 'computer') {
+        playSound('./assets/sounds/lose.mp3');
+    }
+    else {
+        playSound('./assets/sounds/tie.mp3');
+    }
+
 }
 
 function setStatus(element, isWinner) {
